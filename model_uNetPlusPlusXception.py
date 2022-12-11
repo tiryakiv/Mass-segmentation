79c5912817f9443b05cf278f9b905dfdbec7e211
Volkan Müjdat Tiryaki <48734237+tiryakiv@users.noreply.github.com>
2022-12-11 18:08:22 +0300

    Add files via upload

---
+++ model_uNetPlusPlusXception.py
@@ -0,0 +1,134 @@
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input, ZeroPadding2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import Xception
+
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras import backend as K
+import tensorflow as tf
+from tensorflow.keras.losses import *
+
+def dice_coef(y_true, y_pred):
+    smooth = 1e-15
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coef(y_true, y_pred)
+    return loss
+
+def standard_unit(input_tensor, stage, nb_filter, kernel_size=3):
+
+    x = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv'+stage+'_1', kernel_initializer = 'he_normal', padding='same')(input_tensor)
+    x = BatchNormalization()(x) 
+    x = Dropout(dropout_rate, name='dp'+stage+'_1')(x)
+    x = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv'+stage+'_2', kernel_initializer = 'he_normal', padding='same')(x)
+    x = BatchNormalization()(x) 
+    x = Dropout(dropout_rate, name='dp'+stage+'_2')(x)
+
+    return x
+
+dropout_rate = 0.2
+act = "relu"
+
+def uNetPlusPlusXception(num_top_filter=16, learning_rate=1e-4, input_shape = (640,640,3), num_class=1, deep_supervision=False):
+    inputs = Input(input_shape)
+
+    # Load Pre-trained Xception Model
+    xception_pt = Xception(include_top=False, weights="imagenet", input_tensor=inputs)
+    #xception_pt.summary()
+    
+    # skip connections from Xception model
+    s1_1 = xception_pt.layers[0].output        # shape= (640,640,3)
+    s2_1 = xception_pt.layers[11].output
+    s2_1 = ZeroPadding2D(((3,0),(3,0)))(s2_1)  # shape= (320,320,128)
+    s3_1 = xception_pt.layers[21].output
+    s3_1 = ZeroPadding2D(((1,0),(1,0)))(s3_1)  # shape= (160,160,256)
+    s4_1 = xception_pt.layers[31].output       # shape= (80,80,728)
+    s5_1 = xception_pt.layers[121].output      # shape= (40,40,1024)
+    
+    # UnetPlusPlus original implementation 
+    bn_axis = 3
+
+    nb_filter = [num_top_filter,num_top_filter*2,num_top_filter*4,num_top_filter*8,num_top_filter*16]
+    
+    conv1_1 = s1_1  #standard_unit(img_input, stage='11', nb_filter=nb_filter[0])
+    pool1 = MaxPooling2D((2, 2), strides=(2, 2), name='pool1')(conv1_1)
+
+    conv2_1 = s2_1   #standard_unit(pool1, stage='21', nb_filter=nb_filter[1])
+    pool2 = MaxPooling2D((2, 2), strides=(2, 2), name='pool2')(conv2_1)
+
+    up1_2 = Conv2DTranspose(nb_filter[0], (2, 2), strides=(2, 2), name='up12', padding='same')(conv2_1)
+    conv1_2 = concatenate([up1_2, conv1_1], name='merge12', axis=bn_axis)
+    conv1_2 = standard_unit(conv1_2, stage='12', nb_filter=nb_filter[0])
+
+    conv3_1 = s3_1   #standard_unit(pool2, stage='31', nb_filter=nb_filter[2])
+    pool3 = MaxPooling2D((2, 2), strides=(2, 2), name='pool3')(conv3_1)
+
+    up2_2 = Conv2DTranspose(nb_filter[1], (2, 2), strides=(2, 2), name='up22', padding='same')(conv3_1)
+    conv2_2 = concatenate([up2_2, conv2_1], name='merge22', axis=bn_axis)
+    conv2_2 = standard_unit(conv2_2, stage='22', nb_filter=nb_filter[1])
+
+    up1_3 = Conv2DTranspose(nb_filter[0], (2, 2), strides=(2, 2), name='up13', padding='same')(conv2_2)
+    conv1_3 = concatenate([up1_3, conv1_1, conv1_2], name='merge13', axis=bn_axis)
+    conv1_3 = standard_unit(conv1_3, stage='13', nb_filter=nb_filter[0])
+
+    conv4_1 = s4_1  #standard_unit(pool3, stage='41', nb_filter=nb_filter[3])
+    pool4 = MaxPooling2D((2, 2), strides=(2, 2), name='pool4')(conv4_1)
+
+    up3_2 = Conv2DTranspose(nb_filter[2], (2, 2), strides=(2, 2), name='up32', padding='same')(conv4_1)
+    conv3_2 = concatenate([up3_2, conv3_1], name='merge32', axis=bn_axis)
+    conv3_2 = standard_unit(conv3_2, stage='32', nb_filter=nb_filter[2])
+
+    up2_3 = Conv2DTranspose(nb_filter[1], (2, 2), strides=(2, 2), name='up23', padding='same')(conv3_2)
+    conv2_3 = concatenate([up2_3, conv2_1, conv2_2], name='merge23', axis=bn_axis)
+    conv2_3 = standard_unit(conv2_3, stage='23', nb_filter=nb_filter[1])
+
+    up1_4 = Conv2DTranspose(nb_filter[0], (2, 2), strides=(2, 2), name='up14', padding='same')(conv2_3)
+    conv1_4 = concatenate([up1_4, conv1_1, conv1_2, conv1_3], name='merge14', axis=bn_axis)
+    conv1_4 = standard_unit(conv1_4, stage='14', nb_filter=nb_filter[0])
+
+    conv5_1 = s5_1 #standard_unit(pool4, stage='51', nb_filter=nb_filter[4])
+
+    up4_2 = Conv2DTranspose(nb_filter[3], (2, 2), strides=(2, 2), name='up42', padding='same')(conv5_1)
+    conv4_2 = concatenate([up4_2, conv4_1], name='merge42', axis=bn_axis)
+    conv4_2 = standard_unit(conv4_2, stage='42', nb_filter=nb_filter[3])
+
+    up3_3 = Conv2DTranspose(nb_filter[2], (2, 2), strides=(2, 2), name='up33', padding='same')(conv4_2)
+    conv3_3 = concatenate([up3_3, conv3_1, conv3_2], name='merge33', axis=bn_axis)
+    conv3_3 = standard_unit(conv3_3, stage='33', nb_filter=nb_filter[2])
+
+    up2_4 = Conv2DTranspose(nb_filter[1], (2, 2), strides=(2, 2), name='up24', padding='same')(conv3_3)
+    conv2_4 = concatenate([up2_4, conv2_1, conv2_2, conv2_3], name='merge24', axis=bn_axis)
+    conv2_4 = standard_unit(conv2_4, stage='24', nb_filter=nb_filter[1])
+
+    up1_5 = Conv2DTranspose(nb_filter[0], (2, 2), strides=(2, 2), name='up15', padding='same')(conv2_4)
+    conv1_5 = concatenate([up1_5, conv1_1, conv1_2, conv1_3, conv1_4], name='merge15', axis=bn_axis)
+    conv1_5 = standard_unit(conv1_5, stage='15', nb_filter=nb_filter[0])
+
+    nestnet_output_1 = Conv2D(num_class, (1, 1), activation='sigmoid', name='output_1', kernel_initializer = 'he_normal', padding='same')(conv1_2)
+    nestnet_output_2 = Conv2D(num_class, (1, 1), activation='sigmoid', name='output_2', kernel_initializer = 'he_normal', padding='same')(conv1_3)
+    nestnet_output_3 = Conv2D(num_class, (1, 1), activation='sigmoid', name='output_3', kernel_initializer = 'he_normal', padding='same')(conv1_4)
+    nestnet_output_4 = Conv2D(num_class, (1, 1), activation='sigmoid', name='output_4', kernel_initializer = 'he_normal', padding='same')(conv1_5)
+
+    #print(nestnet_ouptput_1.shape)
+    #print(nestnet_ouptput_2.shape)
+    #print(nestnet_ouptput_3.shape)
+    #print(nestnet_ouptput_4.shape)
+    
+    if deep_supervision:
+        model = Model(inputs=inputs, outputs=[nestnet_output_1,
+                                               nestnet_output_2,
+                                               nestnet_output_3,
+                                               nestnet_output_4])
+    else:
+        model = Model(inputs=inputs, outputs=[nestnet_output_4])
+        
+    return model
